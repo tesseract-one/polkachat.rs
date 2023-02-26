bc244c5e99a1f695ecbdf2bcf1cd8baff7206064
Daniel Leping <dileping@gmail.com>
2023-02-26 21:53:12 +0000

    isolated iOS build dependencies

--- common/build.rs
+++ common/build.rs
@@ -1,19 +1,27 @@
-extern crate cbindgen;
+#[cfg(target_os = "ios")]
+mod ios {
+    extern crate cbindgen;
 
-use std::env;
-use std::path::Path;
+    use std::env;
+    use std::path::Path;
+    
+    fn run() {
+        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let profile = env::var("PROFILE").unwrap();
+        let header_path = Path::new(&crate_dir)
+            .join("..")
+            .join("target")
+            .join(&profile)
+            .join("include")
+            .join("polkachat.h");
+    
+        cbindgen::generate(&crate_dir)
+            .expect("Unable to generate bindings")
+            .write_to_file(&header_path);
+    }
+}
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let profile = env::var("PROFILE").unwrap();
-    let header_path = Path::new(&crate_dir)
-        .join("..")
-        .join("target")
-        .join(&profile)
-        .join("include")
-        .join("polkachat.h");
-
-    cbindgen::generate(&crate_dir)
-        .expect("Unable to generate bindings")
-        .write_to_file(&header_path);
+    #[cfg(target_os = "ios")]
+    ios::run()
 }
