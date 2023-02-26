dd6230077052fbcc80529c8d5243faf19bc3b192
Daniel Leping <dileping@gmail.com>
2023-02-26 22:05:01 +0000

    Proper iOS conditioning for build.rs

--- common/build.rs
+++ common/build.rs
@@ -4,16 +4,19 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let profile = env::var("PROFILE").unwrap();
-    let header_path = Path::new(&crate_dir)
-        .join("..")
-        .join("target")
-        .join(&profile)
-        .join("include")
-        .join("polkachat.h");
+    let os = build_target::target_os().unwrap();
+    if os == build_target::Os::iOs {
+        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let profile = env::var("PROFILE").unwrap();
+        let header_path = Path::new(&crate_dir)
+            .join("..")
+            .join("target")
+            .join(&profile)
+            .join("include")
+            .join("polkachat.h");
 
-    cbindgen::generate(&crate_dir)
-        .expect("Unable to generate bindings")
-        .write_to_file(&header_path);
+        cbindgen::generate(&crate_dir)
+            .expect("Unable to generate bindings")
+            .write_to_file(&header_path);
+    }
 }
