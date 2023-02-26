af958c8b54cc744345a5c169b922ef3bce7a431f
Daniel Leping <dileping@gmail.com>
2023-02-26 21:59:30 +0000

    Revert "isolated iOS build dependencies"
    This reverts commit bc244c5e99a1f695ecbdf2bcf1cd8baff7206064.

--- common/build.rs
+++ common/build.rs
@@ -1,27 +1,19 @@
-#[cfg(target_os = "ios")]
-mod ios {
-    extern crate cbindgen;
+extern crate cbindgen;
 
-    use std::env;
-    use std::path::Path;
-    
-    fn run() {
-        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-        let profile = env::var("PROFILE").unwrap();
-        let header_path = Path::new(&crate_dir)
-            .join("..")
-            .join("target")
-            .join(&profile)
-            .join("include")
-            .join("polkachat.h");
-    
-        cbindgen::generate(&crate_dir)
-            .expect("Unable to generate bindings")
-            .write_to_file(&header_path);
-    }
-}
+use std::env;
+use std::path::Path;
 
 fn main() {
-    #[cfg(target_os = "ios")]
-    ios::run()
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let profile = env::var("PROFILE").unwrap();
+    let header_path = Path::new(&crate_dir)
+        .join("..")
+        .join("target")
+        .join(&profile)
+        .join("include")
+        .join("polkachat.h");
+
+    cbindgen::generate(&crate_dir)
+        .expect("Unable to generate bindings")
+        .write_to_file(&header_path);
 }
