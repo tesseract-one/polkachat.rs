9d7fc7f871e7b56081463a9eb44ae0916715ab2f
Daniel Leping <dileping@gmail.com>
2023-03-08 01:32:57 +0000

    scroll on resize in iOS messages view

--- ios/PolkaChat/UI/MessagesView.swift
+++ ios/PolkaChat/UI/MessagesView.swift
@@ -11,33 +11,40 @@ struct MessagesView: View {
     let messages: Array<Message>
     
     var body: some View {
-        ScrollViewReader { scrollView in
-            ScrollView {
-                LazyVStack(alignment: .leading) {
-                    ForEach(messages) { message in
-                        HStack {
-                            if case .submitted(id: _, text: _) = message {
-                                ProgressView().padding(.trailing, 2)
+        GeometryReader { geometry in
+            ScrollViewReader { scrollView in
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(messages) { message in
+                            HStack {
+                                if case .submitted(id: _, text: _) = message {
+                                    ProgressView().padding(.trailing, 2)
+                                }
+                                Text(message.text)
                             }
-                            Text(message.text)
+                            .id(message.id)
+                            .padding(6)
+                            .padding(.horizontal, 4)
+                            .background(Color(red: 0x8F/0xFF,
+                                              green: 0xB8/0xFF,
+                                              blue: 0xE3/0xFF))
+                            .cornerRadius(32)
+                            .padding(.vertical, 2)
                         }
-                        .id(message.id)
-                        .padding(6)
-                        .padding(.horizontal, 4)
-                        .background(Color(red: 0x8F/0xFF,
-                                          green: 0xB8/0xFF,
-                                          blue: 0xE3/0xFF))
-                        .cornerRadius(32)
-                        .padding(.vertical, 2)
                     }
-                }
-                .onChange(of: messages) { messages in
-                    withAnimation(.easeInOut(duration: 60)) {
+                    .onChange(of: messages) { messages in
+                        withAnimation(.easeInOut(duration: 60)) {
+                            scrollView.scrollTo(messages.last?.id)
+                        }
+                    }
+                    .onChange(of: Int(geometry.size.height)) { _ in
+                        withAnimation(.easeInOut(duration: 60)) {
+                            scrollView.scrollTo(messages.last?.id)
+                        }
+                    }
+                    .onAppear {
                         scrollView.scrollTo(messages.last?.id)
                     }
-                }
-                .onAppear {
-                    scrollView.scrollTo(messages.last?.id)
                 }
             }
         }
