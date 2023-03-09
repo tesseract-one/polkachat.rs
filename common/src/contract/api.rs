3a422d3bbbd69260db99559b8707e383578ac19b
Yehor Popovych <popovych.yegor@gmail.com>
2023-03-09 13:25:33 +0000

    updated smart contract

--- common/src/contract/api.rs
+++ common/src/contract/api.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 use sp_weights::Weight;
 use subxt::{
     ext::codec::Decode, ext::sp_core::Bytes, ext::sp_runtime::AccountId32, rpc_params, tx::Signer,
-    Config, OnlineClient, PolkadotConfig, Metadata, Error
+    Config, Error, Metadata, OnlineClient, PolkadotConfig,
 };
 
 use crate::substrate::OnlineClientWebPKI;
@@ -30,36 +30,41 @@ mod contract {
         pub const LEN: &'static str = "0x839b3548";
     }
 
+    // Can be imported form ink_primitives but added here to reduce dependencies
+    #[non_exhaustive]
+    #[repr(u32)]
+    #[derive(Decode, Debug)]
+    pub enum LangError {
+        CouldNotReadInput,
+    }
+
     pub const GAS_LIMIT: Weight = Weight::from_parts(9_375_000_000, 524288);
 }
 
-pub (crate) struct Api {
+pub(crate) struct Api {
     client: OnlineClient<PolkadotConfig>,
     contract: AccountId32,
 }
 
 //if you need to publish on mainnet, please, just wrap with features
 const API_URL: &str = "wss://rococo-contracts-rpc.polkadot.io:443";
-const SMART_CONTRACT: &str = "5E5UJJ91pVa82RXnteAQV8ERMxZy5wW6fS2MpmRF3GXNpdjE";
+const SMART_CONTRACT: &str = "5GQu322xPcZSLdAPewy8BP9qHeir8Y9cLogbDaiRNuJzCeFD";
 
 impl Api {
-    pub (crate) async fn new() -> Result<Self, Error> {
+    pub(crate) async fn new() -> Result<Self, Error> {
         debug!("Creating Substrate client - start");
         let client = OnlineClient::<PolkadotConfig>::from_url_web_pki(API_URL).await?;
         debug!("Created client successfully");
         let contract = AccountId32::from_str(SMART_CONTRACT)?;
         debug!("Created contract ID");
-        Ok(Self {
-            client,
-            contract,
-        })
+        Ok(Self { client, contract })
     }
 
-    pub (crate) fn metadata(&self) -> Metadata {
+    pub(crate) fn metadata(&self) -> Metadata {
         self.client.metadata()
     }
 
-    pub (crate) async fn get(self: Arc<Self>, from: u32, to: u32) -> Result<Vec<String>, Error> {
+    pub(crate) async fn get(self: Arc<Self>, from: u32, to: u32) -> Result<Vec<String>, Error> {
         let query = ContractCallQuery::<<PolkadotConfig as Config>::AccountId>::new_call(
             self.contract.clone(),
             self.contract.clone(),
@@ -74,10 +79,14 @@ impl Api {
         let at: Option<<PolkadotConfig as Config>::Hash> = None;
         let params = rpc_params!["ContractsApi_call", query.as_param(), at];
 
-        let response = self.client.rpc().request::<Bytes>("state_call", params).await?;
+        let response = self
+            .client
+            .rpc()
+            .request::<Bytes>("state_call", params)
+            .await?;
 
-        let value: Vec<String> = parse_query_result(response)?.0;
-        Ok(value)
+        let value: Result<Vec<String>, contract::LangError> = parse_query_result(response)?.0;
+        Ok(value.map_err(|e| format!("{:?}", e))?)
     }
 
     pub async fn len(&self) -> Result<u32, Error> {
@@ -92,10 +101,14 @@ impl Api {
         let at: Option<<PolkadotConfig as Config>::Hash> = None;
         let params = rpc_params!["ContractsApi_call", query.as_param(), at];
 
-        let response = self.client.rpc().request::<Bytes>("state_call", params).await?;
+        let response = self
+            .client
+            .rpc()
+            .request::<Bytes>("state_call", params)
+            .await?;
 
-        let value = parse_query_result(response)?.0;
-        Ok(value)
+        let value: Result<u32, contract::LangError> = parse_query_result(response)?.0;
+        Ok(value.map_err(|e| format!("{:?}", e))?)
     }
 
     pub async fn add<S>(&self, text: &str, signer: &S) -> Result<(), Error>
